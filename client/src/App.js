2f4b0b52e7ef0dd3c080ec5586b38626e8232f63
Esteban742 <freddyesteban77@gmail.com>
2025-09-13 23:22:18 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,41 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./Components/Pages/IndexPage/Index";
 import Login from "./Components/Pages/LoginPage/Login";
 import Register from "./Components/Pages/RegisterPage/Register";
 import Boards from "./Components/Pages/BoardsPage/Boards";
 import Board from "./Components/Pages/BoardPage/Board";
-
 import Alert from "./Components/AlertSnackBar";
+
 import ProtectedRoute from "./Utils/ProtectedRoute";
 import FreeRoute from "./Utils/FreeRoute";
 
-import Store from "./Redux/Store";
 import { loadUser } from "./Services/userService";
+import Store from "./Redux/Store";
+import setBearer from "./Utils/setBearer";
 
 const App = () => {
   useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) setBearer(token);
     loadUser(Store.dispatch);
   }, []);
 
   return (
     <BrowserRouter>
       <Alert />
-      <Switch>
-        <ProtectedRoute exact path="/boards" component={Boards} />
-        <ProtectedRoute exact path="/board/:id" component={Board} />
-        <FreeRoute exact path="/login" component={Login} />
-        <FreeRoute exact path="/register" component={Register} />
-        <FreeRoute exact path="/" component={Index} />
-      </Switch>
+      <Routes>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/boards" element={<Boards />} />
+          <Route path="/board/:id" element={<Board />} />
+        </Route>
+        <Route element={<FreeRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Index />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
