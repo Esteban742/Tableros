f54e8b77c1c4f8ce78d704b9bbf26c7c677c1652
Esteban742 <freddyesteban77@gmail.com>
2025-09-13 23:22:55 -0500

    Update userService.js

--- client/src/Services/userService.js
+++ client/src/Services/userService.js
@@ -5,39 +5,28 @@ import {
   loginStart,
   loginFailure,
   loginSuccess,
+  loadStart,
   loadSuccess,
   loadFailure,
-  loadStart,
   fetchingStart,
   fetchingFinish,
 } from "../Redux/Slices/userSlice";
 import { openAlert } from "../Redux/Slices/alertSlice";
 import setBearer from "../Utils/setBearer";
 
-// Base URL dinámico según entorno
 const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://trello-clone-mern-ggrz.onrender.com/user/"
     : "http://localhost:3001/user/";
 
 // Registro
-export const register = async (
-  { name, surname, email, password, repassword },
-  dispatch
-) => {
+export const register = async (data, dispatch) => {
   dispatch(registrationStart());
-  if (password !== repassword) {
-    dispatch(
-      openAlert({ message: "Your passwords do not match!", severity: "error" })
-    );
+  if (data.password !== data.repassword) {
+    dispatch(openAlert({ message: "Passwords do not match!", severity: "error" }));
   } else {
     try {
-      const res = await axios.post(`${baseUrl}register`, {
-        name,
-        surname,
-        email,
-        password,
-      });
+      const res = await axios.post(`${baseUrl}register`, data);
       dispatch(
         openAlert({
           message: res.data.message,
@@ -47,14 +36,10 @@ export const register = async (
         })
       );
     } catch (error) {
-      dispatch(
-        openAlert({
-          message: error?.response?.data?.errMessage
-            ? error.response.data.errMessage
-            : error.message,
-          severity: "error",
-        })
-      );
+      dispatch(openAlert({
+        message: error?.response?.data?.errMessage || error.message,
+        severity: "error",
+      }));
     }
   }
   dispatch(registrationEnd());
@@ -64,39 +49,32 @@ export const register = async (
 export const login = async ({ email, password }, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post(baseUrl + "login", { email, password });
+    const res = await axios.post(`${baseUrl}login`, { email, password });
     const { user, message } = res.data;
 
     localStorage.setItem("token", user.token);
     setBearer(user.token);
 
     dispatch(loginSuccess({ user }));
-    dispatch(
-      openAlert({ message, severity: "success", duration: 500, nextRoute: "/boards" })
-    );
+    dispatch(openAlert({ message, severity: "success", nextRoute: "/boards" }));
   } catch (error) {
     dispatch(loginFailure());
-    dispatch(
-      openAlert({
-        message: error?.response?.data?.errMessage
-          ? error.response.data.errMessage
-          : error.message,
-        severity: "error",
-      })
-    );
+    dispatch(openAlert({
+      message: error?.response?.data?.errMessage || error.message,
+      severity: "error",
+    }));
   }
 };
 
-// Cargar usuario logueado
+// Cargar usuario
 export const loadUser = async (dispatch) => {
   dispatch(loadStart());
   const token = localStorage.getItem("token");
   if (!token) return dispatch(loadFailure());
 
   setBearer(token);
-
   try {
-    const res = await axios.get(baseUrl + "get-user");
+    const res = await axios.get(`${baseUrl}get-user`);
     dispatch(loadSuccess({ user: res.data }));
   } catch (error) {
     dispatch(loadFailure());
@@ -107,30 +85,21 @@ export const loadUser = async (dispatch) => {
 export const getUserFromEmail = async (email, dispatch) => {
   dispatch(fetchingStart());
   if (!email) {
-    dispatch(
-      openAlert({
-        message: "Please write an email to invite",
-        severity: "warning",
-      })
-    );
+    dispatch(openAlert({ message: "Please write an email to invite", severity: "warning" }));
     dispatch(fetchingFinish());
     return null;
   }
-
   try {
-    const res = await axios.post(baseUrl + "get-user-with-email", { email });
+    const res = await axios.post(`${baseUrl}get-user-with-email`, { email });
     dispatch(fetchingFinish());
     return res.data;
   } catch (error) {
-    dispatch(
-      openAlert({
-        message: error?.response?.data?.errMessage
-          ? error.response.data.errMessage
-          : error.message,
-        severity: "error",
-      })
-    );
+    dispatch(openAlert({
+      message: error?.response?.data?.errMessage || error.message,
+      severity: "error",
+    }));
     dispatch(fetchingFinish());
     return null;
   }
 };
+
