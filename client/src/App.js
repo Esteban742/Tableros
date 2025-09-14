fd81efee95f855e2a9c155e4c13fd2738257a2c7
Esteban742 <freddyesteban77@gmail.com>
2025-09-13 22:50:20 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,146 @@
-import React, { useEffect } from "react";
-import Index from "./Components/Pages/IndexPage/Index";
-import Login from "./Components/Pages/LoginPage/Login";
-import Register from "./Components/Pages/RegisterPage/Register";
-import Alert from "./Components/AlertSnackBar";
-import { BrowserRouter, Switch } from "react-router-dom";
-import Boards from "./Components/Pages/BoardsPage/Boards";
-import ProtectedRoute from "./Utils/ProtectedRoute";
-import { loadUser } from "./Services/userService";
-import Store from "./Redux/Store";
-import FreeRoute from "./Utils/FreeRoute";
-import Board from "./Components/Pages/BoardPage/Board";
-const App = () => {
-  useEffect(() => {
-    loadUser(Store.dispatch);
-  }, []);
-  return (
-    <BrowserRouter>
-      <Alert />
-      <Switch>
-        <ProtectedRoute exact path="/boards" component={Boards} />
-        <ProtectedRoute exact path="/board/:id" component={Board} />
-        <FreeRoute exact path="/login" component={Login} />
-        <FreeRoute exact path="/register" component={Register} />
-        <FreeRoute exact path="/" component={Index} />
-      </Switch>
-    </BrowserRouter>
-  );
+import axios from "axios";
+import {
+  registrationStart,
+  registrationEnd,
+  loginStart,
+  loginFailure,
+  loginSuccess,
+  loadSuccess,
+  loadFailure,
+  loadStart,
+  fetchingStart,
+  fetchingFinish,
+} from "../Redux/Slices/userSlice";
+import { openAlert } from "../Redux/Slices/alertSlice";
+import setBearer from "../Utils/setBearer";
+
+// Base URL dinámico según entorno
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://trello-clone-mern-ggrz.onrender.com/user/"
+    : "http://localhost:3001/user/";
+
+// Registro
+export const register = async (
+  { name, surname, email, password, repassword },
+  dispatch
+) => {
+  dispatch(registrationStart());
+  if (password !== repassword) {
+    dispatch(
+      openAlert({
+        message: "Your passwords do not match!",
+        severity: "error",
+      })
+    );
+  } else {
+    try {
+      const res = await axios.post(`${baseUrl}register`, {
+        name,
+        surname,
+        email,
+        password,
+      });
+      dispatch(
+        openAlert({
+          message: res.data.message,
+          severity: "success",
+          nextRoute: "/login",
+          duration: 1500,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        openAlert({
+          message: error?.response?.data?.errMessage
+            ? error.response.data.errMessage
+            : error.message,
+          severity: "error",
+        })
+      );
+    }
+  }
+  dispatch(registrationEnd());
 };
 
-export default App;
+// Login
+export const login = async ({ email, password }, dispatch) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post(baseUrl + "login", { email, password });
+    const { user, message } = res.data;
+
+    // Guardar token y configurar axios
+    localStorage.setItem("token", user.token);
+    setBearer(user.token);
+
+    dispatch(loginSuccess({ user }));
+    dispatch(
+      openAlert({
+        message,
+        severity: "success",
+        duration: 500,
+        nextRoute: "/boards",
+      })
+    );
+  } catch (error) {
+    dispatch(loginFailure());
+    dispatch(
+      openAlert({
+        message: error?.response?.data?.errMessage
+          ? error.response.data.errMessage
+          : error.message,
+        severity: "error",
+      })
+    );
+  }
+};
+
+// Cargar usuario logueado
+export const loadUser = async (dispatch) => {
+  dispatch(loadStart());
+  const token = localStorage.getItem("token");
+  if (!token) return dispatch(loadFailure());
+
+  setBearer(token);
+
+  try {
+    const res = await axios.get(baseUrl + "get-user");
+    dispatch(loadSuccess({ user: res.data }));
+  } catch (error) {
+    dispatch(loadFailure());
+  }
+};
+
+// Obtener usuario por email
+export const getUserFromEmail = async (email, dispatch) => {
+  dispatch(fetchingStart());
+  if (!email) {
+    dispatch(
+      openAlert({
+        message: "Please write an email to invite",
+        severity: "warning",
+      })
+    );
+    dispatch(fetchingFinish());
+    return null;
+  }
+
+  try {
+    const res = await axios.post(baseUrl + "get-user-with-email", { email });
+    dispatch(fetchingFinish());
+    return res.data;
+  } catch (error) {
+    dispatch(
+      openAlert({
+        message: error?.response?.data?.errMessage
+          ? error.response.data.errMessage
+          : error.message,
+        severity: "error",
+      })
+    );
+    dispatch(fetchingFinish());
+    return null;
+  }
+};
+
