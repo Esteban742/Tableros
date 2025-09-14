fd1d065f4fd0f01fa6b6b933cd4707f2183c9f7b
Esteban742 <freddyesteban77@gmail.com>
2025-09-14 00:53:11 -0500

    Update ProtectedRoute.js

--- client/src/Utils/ProtectedRoute.js
+++ client/src/Utils/ProtectedRoute.js
@@ -3,19 +3,13 @@ import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const { user, loading } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.user);
 
   return (
     <Route
       {...rest}
       render={(props) =>
-        loading ? (
-          <div>Loading...</div>
-        ) : user ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
+        user ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
   );
