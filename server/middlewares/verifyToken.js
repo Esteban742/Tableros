f9ad0d4b43b8959c605fdf91054b84021bee8786
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 15:53:41 -0500

    Update verifyToken.js

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,18 +1,30 @@
+// server/middlewares/verifyToken.js
 const jwt = require("jsonwebtoken");
+const unless = require("express-unless");
 
+// Middleware para verificar token JWT
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) return res.status(403).json("Token inválido");
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(401).json("No estás autenticado");
+  // Se espera que el token venga en la cabecera Authorization
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Formato: "Bearer TOKEN"
+
+  if (!token) {
+    return res.status(403).json({ message: "No token provided." });
   }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Unauthorized!" });
+    }
+
+    // Guardamos la información del usuario decodificada
+    req.user = decoded;
+    next();
+  });
 };
+
+// Esto habilita el uso de .unless()
+verifyToken.unless = unless;
 
 module.exports = verifyToken;
 
