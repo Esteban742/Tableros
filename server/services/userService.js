9b74243c7322c84c0e524060570c90895e5577d9
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 13:26:34 -0500

    Update userService.js

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,63 +1,23 @@
-const userModel = require("../models/userModel");
-const { createRandomHexColor } = require("./helperMethods");
+import axios from "axios";
 
-const register = async (user, callback) => {
-  const newUser = userModel({ ...user, color:createRandomHexColor()});
-  await newUser
-    .save()
-    .then((result) => {
-      return callback(false, { message: "User created successfuly!" });
-    })
-    .catch((err) => {
-      return callback({ errMessage: "Email already in use!", details: err });
-    });
-};
+// Base URL de tu API desplegada
+const API_URL = "https://tableros-53ww.onrender.com/api/users";
 
-const login = async (email, callback) => {
+export const registerUser = async (userData) => {
   try {
-    let user = await userModel.findOne({ email });
-    if (!user) return callback({ errMessage: "Your email/password is wrong!" });
-    return callback(false, { ...user.toJSON() });
-  } catch (err) {
-    return callback({
-      errMessage: "Something went wrong",
-      details: err.message,
-    });
-  }
-};
-
-const getUser = async (id, callback) => {
-  try {
-    let user = await userModel.findById(id);
-    if (!user) return callback({ errMessage: "User not found!" });
-    return callback(false, { ...user.toJSON() });
-  } catch (err) {
-    return callback({
-      errMessage: "Something went wrong",
-      details: err.message,
-    });
-  }
-};
-
-const getUserWithMail = async (email, callback) => {
-  try {
-    let user = await userModel.findOne({ email });
-    if (!user)
-      return callback({
-        errMessage: "There is no registered user with this e-mail.",
-      });
-    return callback(false, { ...user.toJSON() });
+    const response = await axios.post(`${API_URL}/register`, userData);
+    return response.data;
   } catch (error) {
-    return callback({
-      errMessage: "Something went wrong",
-      details: error.message,
-    });
+    // Retorna el mensaje de error para mostrarlo en la UI
+    throw error.response?.data || { errMessage: "Error desconocido" };
   }
 };
 
-module.exports = {
-  register,
-  login,
-  getUser,
-  getUserWithMail,
+export const loginUser = async (loginData) => {
+  try {
+    const response = await axios.post(`${API_URL}/login`, loginData);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { errMessage: "Error desconocido" };
+  }
 };
