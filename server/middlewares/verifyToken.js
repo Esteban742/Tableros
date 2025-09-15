952ccc48425c89ade6d7b73e085eff3e8a15f2e4
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 10:26:00 -0500

    Update verifyToken.js

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,1 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) return res.status(403).json("Token inválido");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("No estás autenticado");
+  }
+};
+
+module.exports = verifyToken;
 
