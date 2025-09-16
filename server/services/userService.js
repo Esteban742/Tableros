1adfbafe2b807dfbadcff0d0849e13d9019056da
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 22:13:51 -0500

    Update userService.js

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,6 +1,6 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const register = async (userData) => {
+const register = async (userData) => {
   try {
     const res = await fetch(`${API_URL}/users/register`, {
       method: "POST",
