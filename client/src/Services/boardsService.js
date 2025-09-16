479576400319c6e81e649c13b00a999441b06cd0
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 23:47:42 -0500

    Update boardsService.js

--- client/src/Services/boardsService.js
+++ client/src/Services/boardsService.js
@@ -9,11 +9,14 @@ import {
   startCreatingBoard,
 } from "../Redux/Slices/boardsSlice";
 import { addNewBoard } from "../Redux/Slices/userSlice";
-import {setLoading, successFetchingBoard, updateTitle} from "../Redux/Slices/boardSlice";
-const baseUrl = "http://localhost:3001/board";
+import { setLoading, successFetchingBoard, updateTitle } from "../Redux/Slices/boardSlice";
 
-export const getBoards = async (fromDropDown,dispatch) => {
-  if(!fromDropDown)dispatch(startFetchingBoards());
+// ✅ Usamos variable de entorno
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
+const baseUrl = `${API_URL}/board`;
+
+export const getBoards = async (fromDropDown, dispatch) => {
+  if (!fromDropDown) dispatch(startFetchingBoards());
   try {
     const res = await axios.get(baseUrl + "/");
     setTimeout(() => {
@@ -67,39 +70,8 @@ export const createBoard = async (props, dispatch) => {
   }
 };
 
-export const getBoard = async (boardId,dispatch) => {
+export const getBoard = async (boardId, dispatch) => {
   dispatch(setLoading(true));
   try {
     const res = await axios.get(baseUrl + "/" + boardId);
-      dispatch(successFetchingBoard(res.data));    
-    setTimeout(() => {
-      dispatch(setLoading(false));      
-    }, 1000);
-  } catch (error) {
-    dispatch(setLoading(false));
-    dispatch(
-      openAlert({
-        message: error?.response?.data?.errMessage
-          ? error.response.data.errMessage
-          : error.message,
-        severity: "error",
-      })
-    );
-  }
-};
-
-export const boardTitleUpdate = async (title, boardId, dispatch) => {
-	try {
-		dispatch(updateTitle(title));
-		await axios.put(baseUrl + '/' + boardId + '/update-board-title', {title:title});
-	} catch (error) {	
-		dispatch(
-			openAlert({
-				message: error?.response?.data?.errMessage ? error.response.data.errMessage : error.message,
-				severity: 'error',
-			})
-		);
-	}
-
-
-};
+    dispatch(successFetchingBoard
