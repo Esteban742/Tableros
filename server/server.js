2d0f4d616dcd35c12bfd1ba2933aee676f7c4161
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 21:06:02 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,9 +36,9 @@ app.use("/api/cards", cardRoute);
 
 // Servir frontend en producción
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "index.html"));
   });
 }
 
