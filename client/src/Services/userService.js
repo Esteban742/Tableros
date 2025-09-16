22f3d8aa38d24426175a2c23537ff31bafb3a4e0
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 23:20:31 -0500

    Update userService.js

--- client/src/Services/userService.js
+++ client/src/Services/userService.js
@@ -1,132 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { register } from "../../../Services/userService";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  BgContainer,
-  Container,
-  TrelloIconContainer,
-  FormSection,
-  FormCard,
-  Form,
-  Title,
-  Input,
-  Button,
-  Icon,
-  Hr,
-  Link,
-} from "./Styled";
+import axios from "axios";
+import { loginStart, loginSuccess, loginFailure } from "../Redux/userSlice";
 
-const Register = () => {
-  let history = useHistory();
-  const dispatch = useDispatch();
-  const { pending } = useSelector((state) => state.user);
+// URL del backend desde .env o localhost
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
-  const [userInformations, setUserInformations] = useState({
-    name: "",
-    surname: "",
-    email: "",
-    password: "",
-    repassword: "",
-  });
-
-  useEffect(() => {
-    document.title = "Registrar Nuevo Usuario";
-  }, []);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // 🔹 Validamos contraseñas en el cliente
-    if (userInformations.password !== userInformations.repassword) {
-      alert("Las contraseñas no coinciden"); 
-      return;
-    }
-
-    // 🔹 Armamos el objeto que espera el backend
-    const userData = {
-      username: `${userInformations.name} ${userInformations.surname}`, 
-      email: userInformations.email,
-      password: userInformations.password,
-    };
-
-    await register(userData, dispatch);
-  };
-
-  return (
-    <>
-      <BgContainer>
-        <Background />
-      </BgContainer>
-      <Container>
-        <TrelloIconContainer onClick={() => history.push("/")}>
-          <Icon src="https://i.postimg.cc/6Qj1y8hB/logok.png" />
-        </TrelloIconContainer>
-        <FormSection>
-          <FormCard>
-            <Form onSubmit={handleSubmit}>
-              <Title>Registrarse</Title>
-              <Input
-                type="text"
-                placeholder="Nombre"
-                required
-                value={userInformations.name}
-                onChange={(e) =>
-                  setUserInformations({ ...userInformations, name: e.target.value })
-                }
-              />
-              <Input
-                type="text"
-                placeholder="Apellido"
-                required
-                value={userInformations.surname}
-                onChange={(e) =>
-                  setUserInformations({ ...userInformations, surname: e.target.value })
-                }
-              />
-              <Input
-                type="email"
-                placeholder="Correo Electrónico"
-                required
-                value={userInformations.email}
-                onChange={(e) =>
-                  setUserInformations({ ...userInformations, email: e.target.value })
-                }
-              />
-              <Input
-                type="password"
-                placeholder="Contraseña"
-                required
-                value={userInformations.password}
-                onChange={(e) =>
-                  setUserInformations({ ...userInformations, password: e.target.value })
-                }
-              />
-              <Input
-                type="password"
-                placeholder="Confirmar Contraseña"
-                required
-                value={userInformations.repassword}
-                onChange={(e) =>
-                  setUserInformations({ ...userInformations, repassword: e.target.value })
-                }
-              />
-
-              <Button type="submit" disabled={pending}>
-                Registrar
-              </Button>
-              <Hr />
-              <Link fontSize="0.85rem" onClick={() => history.push("/login")}>
-                ¿Ya tienes una cuenta? Iniciar Sesión
-              </Link>
-            </Form>
-          </FormCard>
-        </FormSection>
-      </Container>
-    </>
-  );
+// Registrar usuario
+export const register = async (userData, dispatch) => {
+  try {
+    dispatch(loginStart());
+    const res = await axios.post(`${API_URL}/users/register`, userData);
+    dispatch(loginSuccess(res.data));
+    return res.data;
+  } catch (err) {
+    dispatch(loginFailure());
+    throw err;
+  }
 };
 
-export default Register;
+// Login usuario
+export const login = async (credentials, dispatch) => {
+  try {
+    dispatch(loginStart());
+    const res = await axios.post(`${API_URL}/users/login`, credentials);
+    dispatch(loginSuccess(res.data));
+    return res.data;
+  } catch (err) {
+    dispatch(loginFailure());
+    throw err;
+  }
+};
 
 
