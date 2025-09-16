e53cbbd367c9958739dcf99e26e2376346ada614
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 23:33:38 -0500

    Update Login.js

--- client/src/Components/Pages/LoginPage/Login.js
+++ client/src/Components/Pages/LoginPage/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { login } from "../../Services/userService"; // ✅ corregido
+import { login } from "../../../Services/userService"; // ✅ corregido
 import Background from "../../Components/Background"; // ✅ asegúrate que la ruta es correcta
 import {
   BgContainer,
