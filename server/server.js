0c6b68c55381844c5c69a55e998134393f6aa51f
Esteban742 <freddyesteban77@gmail.com>
2025-09-15 21:12:59 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,11 @@ app.use("/api/cards", cardRoute);
 
 // Servir frontend en producción
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 
 
 // MongoDB
