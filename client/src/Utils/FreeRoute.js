e93f49c84b53e7ab676501a6e5b91d780efe6fe5
Esteban742 <freddyesteban77@gmail.com>
2025-09-17 17:46:17 -0500

    Update FreeRoute.js

--- client/src/Utils/FreeRoute.js
+++ client/src/Utils/FreeRoute.js
@@ -1,19 +1,42 @@
-import React from "react";
+// client/src/Utils/FreeRoute.js
+import React, { useEffect, useState } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser } from "../Services/userService";
+import setBearer from "./setBearer";
 
 const FreeRoute = ({ component: Component, ...rest }) => {
+  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setLoading(false);
+      return;
+    }
+
+    setBearer(token);
+
+    (async () => {
+      await loadUser(dispatch);
+      setLoading(false);
+    })();
+  }, [dispatch]);
+
+  if (loading) return <div>Cargando...</div>; // opcional: spinner
 
   return (
     <Route
       {...rest}
       render={(props) =>
-        !user ? <Component {...props} /> : <Redirect to="/boards" />
+        !user || !user.id ? <Component {...props} /> : <Redirect to="/boards" />
       }
     />
   );
 };
 
 export default FreeRoute;
+
 
