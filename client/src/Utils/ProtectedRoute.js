7b22470880599df03f452d781ddffccd8e6ed9e5
Esteban742 <freddyesteban77@gmail.com>
2025-09-17 17:45:06 -0500

    Update ProtectedRoute.js

--- client/src/Utils/ProtectedRoute.js
+++ client/src/Utils/ProtectedRoute.js
@@ -1,15 +1,39 @@
-import React from "react";
+// client/src/Utils/ProtectedRoute.js
+import React, { useEffect, useState } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser } from "../Services/userService";
+import setBearer from "./setBearer";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      setLoading(false); // no hay token, no cargamos usuario
+      return;
+    }
+
+    setBearer(token);
+
+    // Cargar usuario y esperar a que termine
+    (async () => {
+      await loadUser(dispatch);
+      setLoading(false);
+    })();
+  }, [dispatch]);
+
+  if (loading) return <div>Cargando...</div>; // opcional: spinner
 
   return (
     <Route
       {...rest}
       render={(props) =>
-        user ? <Component {...props} /> : <Redirect to="/login" />
+        user && user.id ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
   );
