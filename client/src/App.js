651d4266c9611879b6308da6d4b82d304e3605f1
Esteban742 <freddyesteban77@gmail.com>
2025-09-17 23:08:03 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,58 @@
+// client/src/App.js
 import React, { useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
 import Login from "./Components/Pages/LoginPage/Login";
 import Register from "./Components/Pages/RegisterPage/Register";
 import Boards from "./Components/Pages/BoardsPage/Boards";
 import Board from "./Components/Pages/BoardPage/Board";
 import Alert from "./Components/AlertSnackBar";
+
 import ProtectedRoute from "./Utils/ProtectedRoute";
 import FreeRoute from "./Utils/FreeRoute";
+
 import { loadUser } from "./Services/userService";
 import Store from "./Redux/Store";
 import setBearer from "./Utils/setBearer";
+
 const App = () => {
   useEffect(() => {
-
     const token = localStorage.getItem("token");
 
-    console.log("Token antes de loadUser:", token);
+    if (!token) return; // No hay token, no hacer nada
 
-    if (!token) return;
-    // No hay token, no hacer nada
-    setBearer(token); // primero setear el header
-    console.log("Axios headers:", axios.defaults.headers.common);
-    // Cargar usuario y manejar errores sin romper la app
+    setBearer(token); // Setear token en headers de axios
+
     (async () => {
       const user = await loadUser(Store.dispatch);
       if (!user) console.log("Usuario no cargado al iniciar la app");
     })();
   }, []);
+
   return (
     <BrowserRouter>
       <Alert />
       <Switch>
-        <Route exact path="/" render={() => <Redirect to="/boards" />} />
+        {/* Redirigir raíz */}
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
+
+        {/* Rutas protegidas */}
         <ProtectedRoute exact path="/boards" component={Boards} />
         <ProtectedRoute exact path="/board/:id" component={Board} />
+
+        {/* Rutas libres (login y registro) */}
         <FreeRoute exact path="/login" component={Login} />
         <FreeRoute exact path="/register" component={Register} />
+
+        {/* Catch-all */}
         <Route path="*" render={() => <Redirect to="/" />} />
       </Switch>
     </BrowserRouter>
   );
 };
-export default App;;
+
+export default App;
+
 
 
